18ddbfdb06975a5cc6f82e3c02bb23dc9032ec6e
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-23 21:48:03 +0700

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -1,33 +1,31 @@
-const $idElemCatalogHtml = document.getElementById('catalog_block');
-let elemHtml = '';
+import { variable } from './variable.js';
 
+export function getProducts(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((products) => arrayProduct(products));
+}
 
-export function getProducts () {
-        let url = `https://course-api.com/javascript-store-products`;
-        fetch(url)
-          .then((response) => response.json())
-          .then((products) => arrayProduct(products));
-      }
-      function arrayProduct (products) {
-        localStorage.storage = JSON.stringify(products)
-        let arrayaProductInStorega = JSON.parse(localStorage.storage)
-        arrayaProductInStorega.forEach((element) => {
-            pageCheckForHome(element);
-        });
-      }
-      
-     function pageCheckForHome (productsFeatured) {
-        if (productsFeatured.fields.featured === true) {
-          elemHtml = elemHtml + elementAssembly(productsFeatured);
-        }
-        $idElemCatalogHtml.innerHTML = elemHtml;
-      }
-      
-      function elementAssembly (productsFeatured) {
-        let priceWithDot = roundUpPrice(productsFeatured.fields.price);
-        console.log(productsFeatured.id);
-      
-        let CatalogElemHtml = `
+function arrayProduct(products) {
+  localStorage.storage = JSON.stringify(products);
+  let arrayaProductInStorega = JSON.parse(localStorage.storage);
+  arrayaProductInStorega.forEach((element) => {
+    pageCheckForHome(element);
+  });
+}
+
+function pageCheckForHome(productsFeatured) {
+  if (productsFeatured.fields.featured === true) {
+    variable.elemHtml = variable.elemHtml + elementAssembly(productsFeatured);
+  }
+  variable.$idElemCatalogHtml.innerHTML = variable.elemHtml;
+}
+
+function elementAssembly(productsFeatured) {
+  let priceWithDot = roundUpPrice(productsFeatured.fields.price);
+  console.log(productsFeatured.id);
+
+  let CatalogElemHtml = `
       <div class="card_product_home">
       <div class="home_background_featured" style="Background: url(${productsFeatured.fields.image[0].url}); background-size: cover; object-fit: contain; background-position: center">
       <div class="basket_and_loupe">
@@ -39,14 +37,13 @@ export function getProducts () {
       <p class="product_featured_price">$${priceWithDot}</p>
       </div>
       `;
-        return CatalogElemHtml;
-      }
-      
-      function roundUpPrice (valueForRounding) {
-        let resultValue;
-        let maxValueStartTwoSymble = String(valueForRounding).slice(0, -2);
-        let maxValueEndTwoSymble = String(valueForRounding).slice(-2);
-        resultValue = Number(maxValueStartTwoSymble + '.' + maxValueEndTwoSymble);
-        return resultValue;
-      }
-// export const getProducts =  valueCatalog.getProducts;
+  return CatalogElemHtml;
+}
+
+function roundUpPrice(valueForRounding) {
+  let resultValue;
+  let maxValueStartTwoSymble = String(valueForRounding).slice(0, -2);
+  let maxValueEndTwoSymble = String(valueForRounding).slice(-2);
+  resultValue = Number(maxValueStartTwoSymble + '.' + maxValueEndTwoSymble);
+  return resultValue;
+}
