8956ae508f0da46df90a8ae95a4733abdc1919a0
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-23 21:48:11 +0700

    Update variable.js

--- js/variable.js
+++ js/variable.js
@@ -1,4 +1,6 @@
-variable = {
-    $idElemCatalogHtml: document.getElementById('catalog_block'),
-    elemHtml: ""
-}
+export let variable = {
+  $idElemCatalogHtml: document.getElementById('catalog_block'),
+  elemHtml: '',
+  thisLinkNow: String(window.location.href),
+  url: `https://course-api.com/javascript-store-products`,
+};
