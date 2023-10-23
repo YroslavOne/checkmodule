5921701c4d47983d5dcbe8460e312c8993e452ae
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-23 21:48:07 +0700

    Update module.js

--- js/module.js
+++ js/module.js
@@ -1,2 +1,5 @@
-import {getProducts} from "./catalog.js";
-getProducts();
+import { ifParserFits } from './ParserObj.js';
+import { apiProductCatalog, checkingWhatColorMakesLogo } from './menu.js';
+apiProductCatalog();
+checkingWhatColorMakesLogo(String(window.location.pathname));
+ifParserFits(String(window.location.pathname));
