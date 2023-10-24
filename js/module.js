6de21818e85348b0febca5243e540be7b4517bfa
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-24 15:59:50 +0700

    Update module.js

--- js/module.js
+++ js/module.js
@@ -1,5 +1,11 @@
 import { ifParserFits } from './ParserObj.js';
-import { apiProductCatalog, checkingWhatColorMakesLogo } from './menu.js';
+import { apiProductCatalog, checkingWhatColorMakesLogo, loupeOnClick} from './component/menu.js';
+import {checkLocalStorage} from "./component/card.js"
+
+console.log(localStorage.basketProduct)
+
+checkLocalStorage();
 apiProductCatalog();
 checkingWhatColorMakesLogo(String(window.location.pathname));
 ifParserFits(String(window.location.pathname));
+
