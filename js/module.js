f448c545657561fee7735262829eeeff793beeb0
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-25 14:58:06 +0700

    Update module.js

--- js/module.js
+++ js/module.js
@@ -1,11 +1,19 @@
-import { ifParserFits } from './ParserObj.js';
 import { apiProductCatalog, checkingWhatColorMakesLogo, loupeOnClick} from './component/menu.js';
 import {checkLocalStorage} from "./component/card.js"
-
-console.log(localStorage.basketProduct)
+// import {valueInInput, searchIngredientDrink, foreachProductArray, collectAllData, activator } from "./component/product.js";
+import { getProducts } from './component/catalog.js';
+import { variable } from './variable.js';
+import {loupeRun} from "./component/productLoupe.js"
+ifParserFits(String(window.location.pathname))
+function ifParserFits(urlPageName) {
+  if (urlPageName === '/home.html') {
+        getProducts(variable.url);
+    } else if(urlPageName === '/productsLoupe.html'){
+    loupeRun()
+  }
+}
 
 checkLocalStorage();
 apiProductCatalog();
 checkingWhatColorMakesLogo(String(window.location.pathname));
-ifParserFits(String(window.location.pathname));
 
