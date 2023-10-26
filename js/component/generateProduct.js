851a9a1fe68dc65716c4263208ebda2e8473476a
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-26 16:01:35 +0700

    Update generateProduct.js

--- js/component/generateProduct.js
+++ js/component/generateProduct.js
@@ -8,7 +8,7 @@ import { product } from '../variable.js';
         <div class="home_background_featured" style="Background: url(${productsFeatured.fields.image[0].url}); background-size: cover; object-fit: contain; background-position: center">
         <div class="basket_and_loupe"> 
         <a href="./productsLoupe.html"><button onclick=loupeOnClick("${productsFeatured.id}") class="loupe"><i class="bi bi-search"></i></button></a>
-        <button onclick=toDoBasketOpen("${productsFeatured.id}") class="basket_tap"><i class="bi bi-cart3"></i></button>
+        <button id="prostochek" onclick=toDoBasketOpen("${productsFeatured.id}") class="basket_tap"><i class="bi bi-cart3"></i></button>
         </div>
         </div>
         <h4 class="product_featured_name">${productsFeatured.fields.name}</h4>
@@ -25,3 +25,20 @@ import { product } from '../variable.js';
     resultValue = Number(maxValueStartTwoSymble + '.' + maxValueEndTwoSymble);
     return resultValue;
   }
+
+//   const keksfasdfdsdf = document.getElementById('prostochek')
+
+//   if (keksfasdfdsdf) {keksfasdfdsdf.addEventListener("click",
+//   function () {
+//           Console.log("да бляяяяя");
+//       }, 
+//       false,
+//   );}
+//   window.addEventListener("DOMContentLoaded", (event) => {
+//     const keksfasdfdsdf = document.getElementById('prostochek');
+//     if (keksfasdfdsdf) {
+//         keksfasdfdsdf.addEventListener('click', function () {
+//         Console.log("да бляяяяя");
+//     }, false);
+//     }
+// });
